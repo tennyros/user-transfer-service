fc11838a5cf098d834179c3f2f5c7484f8f95308
Vadim Kamalov <vvkamalov_obs@mail.ru>
2025-06-02 20:22:58 +0500

    init commit

---
+++ src/main/java/com/github/tennyros/transfers/UserTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.tennyros.transfers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTransferServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserTransferServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/tennyros/transfers/UserTransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tennyros.transfers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTransferServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
